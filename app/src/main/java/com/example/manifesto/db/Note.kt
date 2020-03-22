298ea0c86b7b39679f86a348ab4242c6e6ccc42f
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-22 18:59:09 +0530

    Created DAO to access and manipulate the database

--- app/src/main/java/com/example/manifesto/db/Note.kt
+++ app/src/main/java/com/example/manifesto/db/Note.kt
@@ -4,5 +4,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Note {
-}
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id :Int,
+    val title:String,
+    val note:String
+)
