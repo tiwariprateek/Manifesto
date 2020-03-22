cf03e45340ce4e7b98bf393fdcaef46d013e32be
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-22 18:26:09 +0530

    Changes to MainActivity

--- app/src/main/java/com/example/manifesto/ui/HomeFragment.kt
+++ app/src/main/java/com/example/manifesto/ui/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.example.manifesto.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,4 +23,12 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        floatingactionbutton.setOnClickListener {
+            val action=HomeFragmentDirections.actionAddNote()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
--- app/src/main/java/com/example/manifesto/ui/MainActivity.kt
+++ app/src/main/java/com/example/manifesto/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.manifesto.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.manifesto.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController=Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+      return NavigationUI.navigateUp(
+          Navigation.findNavController(this,R.id.fragment),
+          null
+      )
     }
 }
