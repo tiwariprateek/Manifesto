c528539cf43dadd0586a12e59c2c782d6dd81801
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-22 20:29:14 +0530

    Finalised addnote function to save data in database

--- app/src/main/java/com/example/manifesto/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/manifesto/ui/AddNoteFragment.kt
@@ -1,13 +1,17 @@
 package com.example.manifesto.ui
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.example.manifesto.R
+import com.example.manifesto.db.Note
 import com.example.manifesto.db.NoteDatabase
+import kotlinx.android.synthetic.main.fragment_add_note.*
 
 /**
  * A simple [Fragment] subclass.
@@ -25,7 +29,42 @@ class AddNoteFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        NoteDatabase(activity!!).getNoteDao()
+        savebttn.setOnClickListener {
+
+            val noteTitle=edit_text_title.text.toString().trim()
+            val noteBody=edit_text_note.text.toString().trim()
+
+            if(noteTitle.isEmpty()){
+                edit_text_title.error="Title Required"
+                edit_text_title.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(noteBody.isEmpty()){
+                edit_text_note.error="Note Required"
+                edit_text_note.requestFocus()
+                return@setOnClickListener
+            }
+
+            val note=Note(noteTitle,noteBody)
+            savenote(note)
+        }
+
     }
+    private fun savenote(note: Note){
+        class SaveNote:AsyncTask<Void,Void,Void>() {
+            override fun doInBackground(vararg p0: Void?): Void? {
+                NoteDatabase(activity!!).getNoteDao().addnote(note)
+                return null
+            }
+
+            override fun onPostExecute(result: Void?) {
+                super.onPostExecute(result)
+                Toast.makeText(activity,"Note Saved !!",Toast.LENGTH_LONG).show()
+            }
+        }
+        SaveNote().execute()
+    }
+
 
 }
