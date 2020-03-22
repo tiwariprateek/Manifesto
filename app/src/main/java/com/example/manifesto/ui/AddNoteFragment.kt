1c279ce0e6ede869ef73172869d12f5d2328b4ad
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-22 19:42:55 +0530

    Build Room database

--- app/src/main/java/com/example/manifesto/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/manifesto/ui/AddNoteFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.manifesto.R
+import com.example.manifesto.db.NoteDatabase
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +20,12 @@ class AddNoteFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_note, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        NoteDatabase(activity!!).getNoteDao()
     }
 
 }
