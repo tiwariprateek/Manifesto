dc2e2a02d944b1dfa98b58728635cc5f9987247c
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-22 14:38:58 +0530

    Added Fragments and oragnised Files

--- app/src/main/java/com/example/manifesto/ui/MainActivity.kt
+++ app/src/main/java/com/example/manifesto/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.manifesto
+package com.example.manifesto.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.manifesto.R
 
 class MainActivity : AppCompatActivity() {
 
