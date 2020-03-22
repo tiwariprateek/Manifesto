897e13994cb070cd68d1477f45adc12a3b53002c
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-22 19:09:08 +0530

    Created functions inside DAO to insert and view notes

--- app/src/main/java/com/example/manifesto/db/NoteDao.kt
+++ app/src/main/java/com/example/manifesto/db/NoteDao.kt
@@ -1,4 +1,19 @@
 package com.example.manifesto.db
 
+import android.provider.UserDictionary
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface NoteDao {
+
+    @Insert
+    fun addnote(note: Note)
+
+    @Insert
+    fun addMultipleNotes(vararg note: Note)
+
+    @Query("SELECT * FROM note")
+    fun getAllNotes():List<Note>
 }
