82bb52b0c0338343c30906231c8d8368934c9f90
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-23 02:41:42 +0530

    Finshed Recycler view adapter class

--- app/src/main/java/com/example/manifesto/ui/NotesAdapter.kt
+++ app/src/main/java/com/example/manifesto/ui/NotesAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.manifesto.ui
 
-class NotesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.manifesto.R
+import com.example.manifesto.db.Note
+import kotlinx.android.synthetic.main.note_layout.view.*
+
+class NotesAdapter(private val notes:List<Note>):RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(val view: View):RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.note_layout,parent,false)
+        )
+
+    }
+
+    override fun getItemCount()=notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.view.text_view_title.text=notes[position].title
+        holder.view.text_view_desc.text=notes[position].note
+    }
 }
