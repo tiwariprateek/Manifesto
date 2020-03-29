d8984fbea179fbb582c610506555628e53873c5a
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-29 23:38:06 +0530

    Changes

--- app/src/main/java/com/example/manifesto/db/Note.kt
+++ app/src/main/java/com/example/manifesto/db/Note.kt
@@ -3,7 +3,7 @@ package com.example.manifesto.db
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
-
+//Every entity represents a table in the database
 @Entity
 data class Note(
     val title:String,
--- app/src/main/java/com/example/manifesto/ui/Helpers.kt
+++ app/src/main/java/com/example/manifesto/ui/Helpers.kt
@@ -3,5 +3,6 @@ package com.example.manifesto.ui
 import android.content.Context
 import android.widget.Toast
 
+//Extension function to generate toast easier
 fun Context.toast(message:String)=
     Toast.makeText(this,message,Toast.LENGTH_LONG).show()
